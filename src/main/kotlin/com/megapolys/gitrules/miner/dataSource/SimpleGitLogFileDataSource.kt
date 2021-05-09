8d10615f2ee048d1e3719fb921ba629b08827885
Rozhkov Igor <IRozhkov@sberbank.ru>
2021-05-09 18:35:00 +0300

    extracted DataSource interface

--- src/main/kotlin/com/megapolys/gitrules/miner/dataSource/SimpleGitLogFileDataSource.kt
+++ src/main/kotlin/com/megapolys/gitrules/miner/dataSource/SimpleGitLogFileDataSource.kt
@@ -1,9 +1,12 @@
-package com.megapolys.gitrules.miner
+package com.megapolys.gitrules.miner.dataSource
 
+import com.megapolys.gitrules.miner.Commit
 import java.io.File
 
-class DataSource(private val fileName: String) {
-    fun getCommits() =
+class SimpleGitLogFileDataSource(
+    private val fileName: String
+) : DataSource {
+    override fun getCommits() =
         File(fileName)
             .readText()
             .split("\n\n")
