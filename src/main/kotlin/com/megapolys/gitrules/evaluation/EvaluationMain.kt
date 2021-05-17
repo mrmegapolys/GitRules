6d1446af2f1a2147dabce8ee04b97a9848a220d5
Rozhkov Igor <IRozhkov@sberbank.ru>
2021-05-17 23:01:21 +0300

    setting up experiment

--- src/main/kotlin/com/megapolys/gitrules/evaluation/EvaluationMain.kt
+++ src/main/kotlin/com/megapolys/gitrules/evaluation/EvaluationMain.kt
@@ -1,6 +1,8 @@
 package com.megapolys.gitrules.evaluation
 
+import com.megapolys.gitrules.evaluation.strategies.ErrorPreventionEvaluation
 import com.megapolys.gitrules.evaluation.strategies.FalseAlarmEvaluation
+import com.megapolys.gitrules.evaluation.strategies.SourceCodeNavigationEvaluation
 import com.megapolys.gitrules.miner.DataSource
 import com.megapolys.gitrules.miner.fpGrowth.FpGrowth
 import com.megapolys.gitrules.server.RulesService
@@ -16,7 +18,30 @@ fun main() {
         .runWithStatistics(train)
     val rulesService = RulesService(itemsets.levels)
 
-    val experiment = Experiment(FalseAlarmEvaluation(rulesService, minConfidence = 0.95))
-    println(experiment.run(test, chunkSize = 5))
+
+    val navigationExperiment = Experiment(
+        SourceCodeNavigationEvaluation(rulesService)
+    )
+    val preventionExperiment = Experiment(
+        ErrorPreventionEvaluation(rulesService, minConfidence = 0.9)
+    )
+    val falseAlarmExperiment = Experiment(
+        FalseAlarmEvaluation(rulesService, minConfidence = 0.9)
+    )
+
+    println("Starting source code navigation evaluation")
+    val navigationResult = navigationExperiment.run(test, chunkSize = 25)
+    println("Source code navigation evaluation:")
+    println(navigationResult)
+
+    println("Starting error prevention evaluation")
+    val preventionResult = preventionExperiment.run(test, chunkSize = 3)
+    println("Error prevention evaluation:")
+    println(preventionResult)
+
+    println("Starting false alarm evaluation")
+    val falseAlarmResult = falseAlarmExperiment.run(test, chunkSize = 5)
+    println("False alarm evaluation:")
+    println(falseAlarmResult)
 }
 
