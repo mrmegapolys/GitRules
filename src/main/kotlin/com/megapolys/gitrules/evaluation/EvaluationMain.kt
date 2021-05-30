51912ec6216c5335c2c01fceed546bf010c3990f
Rozhkov Igor <IRozhkov@sberbank.ru>
2021-05-30 11:57:47 +0300

    draft

--- src/main/kotlin/com/megapolys/gitrules/evaluation/EvaluationMain.kt
+++ src/main/kotlin/com/megapolys/gitrules/evaluation/EvaluationMain.kt
@@ -7,9 +7,8 @@ import com.megapolys.gitrules.miner.dataSource.SimpleGitLogFileDataSource
 import com.megapolys.gitrules.miner.fpGrowth.FpGrowth
 import com.megapolys.gitrules.server.RulesService
 
-private const val INPUT_FILENAME = "input/th_full.txt"
-private const val MIN_SUPPORT = 8
-
+private const val INPUT_FILENAME = "input/guava.txt"
+private const val MIN_SUPPORT = 12
 fun main() {
     val commits = SimpleGitLogFileDataSource(INPUT_FILENAME).getCommits()
     val (train, test) = commits.run { dropLast(1000) to takeLast(1000) }
@@ -20,27 +19,27 @@ fun main() {
 
 
     val navigationExperiment = Experiment(
-        SourceCodeNavigationEvaluation(rulesService, minConfidence = 0.0)
+        SourceCodeNavigationEvaluation(rulesService, minConfidence = 0.5)
     )
     val preventionExperiment = Experiment(
-        ErrorPreventionEvaluation(rulesService, minConfidence = 0.9)
+        ErrorPreventionEvaluation(rulesService, minConfidence = 0.95)
     )
     val falseAlarmExperiment = Experiment(
-        FalseAlarmEvaluation(rulesService, minConfidence = 0.9)
+        FalseAlarmEvaluation(rulesService, minConfidence = 0.95)
     )
 
     println("Starting source code navigation evaluation")
-    val navigationResult = navigationExperiment.run(test, chunkSize = 25)
+    val navigationResult = navigationExperiment.run(test, chunkSize = 125)
     println("Source code navigation evaluation:")
     println(navigationResult)
 
     println("Starting error prevention evaluation")
-    val preventionResult = preventionExperiment.run(test, chunkSize = 3)
+    val preventionResult = preventionExperiment.run(test, chunkSize = 125)
     println("Error prevention evaluation:")
     println(preventionResult)
 
     println("Starting false alarm evaluation")
-    val falseAlarmResult = falseAlarmExperiment.run(test, chunkSize = 5)
+    val falseAlarmResult = falseAlarmExperiment.run(test, chunkSize = 125)
     println("False alarm evaluation:")
     println(falseAlarmResult)
 }
